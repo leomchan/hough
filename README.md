a58fca2513acd4ec6ec347736b99ea227692e690
Leo Chan <595676+leomchan@users.noreply.github.com>
2022-12-11 14:35:23 -0700

    Initial commit
    Another attempt at hough

---
+++ README.md
@@ -0,0 +1,10 @@
+# Lego studs
+
+Some experiments using opencv2 to identify studs in images of lego bricks.
+
+## Setup
+
+- Python version is managed by [pyenv](https://github.com/pyenv/pyenv).
+- Setup a [venv](https://docs.python.org/3/library/venv.html) virtual
+environment.
+- Install from requirements. `pip install -r requirement.txt`.
---
+++ hough.py
@@ -0,0 +1,34 @@
+# Hough transform to identify circles
+
+import numpy as np
+import cv2
+
+# Read image
+img = cv2.imread('lego.png')
+# Smooth it
+img = cv2.medianBlur(img, 3)
+
+img_copy = img.copy()
+# Convert to greyscale
+img_gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
+
+# Apply Hough transform to greyscale image
+minDist = 100
+param1 = 500
+param2 = 200  # smaller value-> more false circles
+minRadius = 5
+maxRadius = 10
+circles = cv2.HoughCircles(
+    img_gray, cv2.HOUGH_GRADIENT, 1, minDist,
+    param1, param2, minRadius, maxRadius)
+circles = np.uint16(np.around(circles))
+
+# Draw the circles
+for i in circles[0, :]:
+    # draw the outer circle
+    cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    # draw the center of the circle
+    cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+cv2.imshow('detected circles', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
